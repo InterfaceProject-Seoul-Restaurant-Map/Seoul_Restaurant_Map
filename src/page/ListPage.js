da023053a215a53f54561823652bf9738110cb95
chiwon <dh5277@naver.com>
2023-11-08 22:10:24 +0900

    rm

--- src/page/ListPage.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const List = () => {
-    return (
-        <div>List page</div>
-    )
-};
-
-export default List;
--- src/page/RequestPage.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Request = () => {
-    return (
-        <div>request page</div>
-    )
-};
-
-export default Request;
